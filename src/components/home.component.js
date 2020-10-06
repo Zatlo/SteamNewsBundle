a3ebcdad1db9be8a26e1c11762aa23ebf95e6ff3
B00M6614 <jtiscareno@ucmerced.edu>
2020-10-05 17:06:23 -0700

    working on home page

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,13 +1,16 @@
 import React, {Component } from 'react';
 //import {Link} from 'react-router-dom';
 //import axios from 'axios';
+import "./home.component.css";
 
 
 export default class HomePage extends Component {
     render() {
         return (
-            <div>
-                <p> Home Page!</p>
+            <div class="jumbotron">
+                <div class="container">
+                    <h1>Steam News on Gaming Bundes</h1>
+                </div>
             </div>
         )
     }
